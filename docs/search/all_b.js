c4d4b14e7ecafd5297f2a512d2003ea2e560395b
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2024-06-12 18:27:51 +0000

    Deploying to gh-pages from @ knncolle/knncolle_annoy@f2062ee7764d2864092b675b4df39148a8fcfe43 🚀

--- docs/search/all_b.js
+++ docs/search/all_b.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['overview_0',['Overview',['../md__2github_2workspace_2README.html#autotoc_md1',1,'']]]
+  ['on_20vectorization_0',['Note on vectorization',['../md__2github_2workspace_2README.html#autotoc_md7',1,'']]],
+  ['overview_1',['Overview',['../md__2github_2workspace_2README.html#autotoc_md1',1,'']]]
 ];
