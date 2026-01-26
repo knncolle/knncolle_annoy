45973af698731ff97a857d8a4a52c047273e0e36
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2026-01-26 15:53:28 +1100

    Reorganize source code and tests into multiple files.

--- tests/src/Annoy.cpp
+++ tests/src/Annoy.cpp
@@ -1,65 +1,14 @@
 #include <gtest/gtest.h>
-#include "knncolle_annoy/knncolle_annoy.hpp"
 
 #include <random>
 #include <vector>
-#include <filesystem>
-#include <cstring>
+#include <algorithm>
+#include <tuple>
+#include <cmath>
 
-class TestCore {
-protected:
-    inline static int nobs, ndim;
-    inline static std::vector<double> data;
-    inline static std::tuple<int, int> last_params;
+#include "knncolle_annoy/Annoy.hpp"
 
-protected:
-    static void assemble(const std::tuple<int, int>& param) {
-        if (param == last_params) {
-            return;
-        }
-        last_params = param;
-
-        nobs = std::get<0>(param);
-        ndim = std::get<1>(param);
-
-        std::mt19937_64 rng(nobs * 10 + ndim);
-        std::normal_distribution distr;
-
-        data.resize(nobs * ndim);
-        for (auto& d : data) {
-            d = distr(rng);
-        }
-    }
-
-    template<class It_, class Rng_>
-    static void fill_random(It_ start, It_ end, Rng_& eng) {
-        std::normal_distribution distr;
-        while (start != end) {
-            *start = distr(eng);
-            ++start;
-        }
-    }
-
-protected:
-    static void sanity_checks(const std::vector<int>& indices, const std::vector<double>& distances) {
-        EXPECT_TRUE(std::is_sorted(distances.begin(), distances.end())); // sorted by increasing distance.
-
-        auto sorted = indices;
-        std::sort(sorted.begin(), sorted.end());
-        EXPECT_TRUE(std::adjacent_find(sorted.begin(), sorted.end()) == sorted.end()); // all neighbors are unique.
-    }
-
-    static void sanity_checks(const std::vector<int>& indices, const std::vector<double>& distances, int k, int self) { // for finding by index
-        EXPECT_EQ(indices.size(), distances.size());
-        EXPECT_EQ(indices.size(), std::min(k, nobs - 1));
-
-        for (const auto& ix : indices) { // self is not in there.
-            EXPECT_TRUE(ix != self);
-        }
-
-        sanity_checks(indices, distances);
-    }
-};
+#include "TestCore.h"
 
 class AnnoyTest : public TestCore, public ::testing::TestWithParam<std::tuple<std::tuple<int, int>, int> > { 
 protected:
@@ -351,89 +300,4 @@ TEST(Annoy, Duplicates) {
         EXPECT_EQ(dres2.back(), 0);
         EXPECT_EQ(dres2.front(), 0);
     }
-}
-
-#include "knncolle_annoy/load_annoy_prebuilt.hpp"
-
-class AnnoyLoadPrebuiltTest : public TestCore, public ::testing::Test {
-protected:
-    inline static std::filesystem::path savedir;
-
-    static void SetUpTestSuite() {
-        assemble({ 50, 5 });
-
-        savedir = "save-prebuilt-tests";
-        std::filesystem::remove_all(savedir);
-        std::filesystem::create_directory(savedir);
-
-        auto& reg = knncolle::load_prebuilt_registry<int, double, double>();
-        reg[knncolle_annoy::save_name] = [](const std::string& prefix) -> knncolle::Prebuilt<int, double, double>* {
-            auto scanned = knncolle_annoy::scan_prebuilt_save_config(prefix);
-            if (std::strcmp(scanned.distance.c_str(), "euclidean") == 0) {
-                return knncolle_annoy::load_annoy_prebuilt<int, double, double, Annoy::Euclidean>(prefix);
-            }
-            return knncolle_annoy::load_annoy_prebuilt<int, double, double, Annoy::Manhattan>(prefix);
-        };
-    }
-};
-
-TEST_F(AnnoyLoadPrebuiltTest, Euclidean) {
-    knncolle_annoy::AnnoyBuilder<int, double, double, Annoy::Euclidean> ab;
-    auto aptr = ab.build_unique(knncolle::SimpleMatrix<int, double>(ndim, nobs, data.data()));
-
-    const auto prefix = (savedir / "euclidean_").string();
-    aptr->save(prefix);
-
-    auto reloaded = knncolle::load_prebuilt_shared<int, double, double>(prefix);
-    std::vector<int> output_i, output_i2;
-    std::vector<double> output_d, output_d2;
-
-    auto searcher = aptr->initialize();
-    auto researcher = reloaded->initialize();
-    for (int x = 0; x < nobs; ++x) {
-        searcher->search(x, 5, &output_i, &output_d);
-        researcher->search(x, 5, &output_i2, &output_d2);
-        EXPECT_EQ(output_i, output_i2);
-        EXPECT_EQ(output_d, output_d2);
-    }
-}
-
-TEST_F(AnnoyLoadPrebuiltTest, Manhattan) {
-    knncolle_annoy::AnnoyBuilder<int, double, double, Annoy::Manhattan> ab;
-    auto aptr = ab.build_unique(knncolle::SimpleMatrix<int, double>(ndim, nobs, data.data()));
-
-    const auto prefix = (savedir / "manhattan_").string();
-    aptr->save(prefix);
-
-    auto reloaded = knncolle::load_prebuilt_shared<int, double, double>(prefix);
-    std::vector<int> output_i, output_i2;
-    std::vector<double> output_d, output_d2;
-
-    auto searcher = aptr->initialize();
-    auto researcher = reloaded->initialize();
-    for (int x = 0; x < nobs; ++x) {
-        searcher->search(x, 5, &output_i, &output_d);
-        researcher->search(x, 5, &output_i2, &output_d2);
-        EXPECT_EQ(output_i, output_i2);
-        EXPECT_EQ(output_d, output_d2);
-    }
-}
-
-TEST_F(AnnoyLoadPrebuiltTest, Error) {
-    knncolle_annoy::AnnoyBuilder<int, double, double, Annoy::Manhattan> ab;
-    auto aptr = ab.build_unique(knncolle::SimpleMatrix<int, double>(ndim, nobs, data.data()));
-
-    const auto prefix = (savedir / "error_").string();
-    aptr->save(prefix);
-
-    // Deleting the index. 
-    std::filesystem::remove(prefix + "index");
-
-    bool failed = false;
-    try {
-        knncolle::load_prebuilt_shared<int, double, double>(prefix);
-    } catch (std::exception& e) {
-        failed = true;
-    }
-    EXPECT_TRUE(failed);
 }
